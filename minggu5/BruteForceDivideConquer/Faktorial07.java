434b87e83c13f7f0b55d02924337599b1456c3f5
okeokke <okeokke77@gmail.com>
2026-03-27 14:08:39 +0700

    Percobaan 1 Part 1.

---
+++ minggu5/BruteForceDivideConquer/Faktorial07.java
@@ -0,0 +1,21 @@
+package asd_job5.minggu5.BruteForceDivideConquer;
+public class Faktorial07 {
+
+  int faktorialBF (int n) {
+    int fakto=1;
+    for (int i = 1; i <= n; i++) {
+      fakto = fakto*i;
+    }
+    return fakto;
+  }
+
+  int faktorialDC (int n) {
+    if (n==1) {
+      return 1;
+    } else {
+      int fakto = n * faktorialDC(n-1);
+      return fakto;
+    }
+  }
+  
+}
---
+++ minggu5/BruteForceDivideConquer/MainFaktorial07.java
@@ -0,0 +1,17 @@
+package asd_job5.minggu5.BruteForceDivideConquer;
+import java.util.Scanner;
+
+public class MainFaktorial07 {
+  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Masukkan nilai : ");
+    int nilai = sc.nextInt();
+
+
+    Faktorial07 fk = new Faktorial07();
+    System.out.println("Nilai Faktorial "+nilai+" menggunakan BF : "+fk.faktorialBF(nilai));
+    System.out.println("Nilai Faktorial "+nilai+" menggunakan DC : "+fk.faktorialDC(nilai));
+  }
+
+}
